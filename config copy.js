0dbc648182ff5920a0c2d9b9332556246599c95d
saihgupr <saihgupr@duck.com>
2025-08-08 08:31:42 -0400

    API in Settings popup

--- config copy.js
+++
@@ -1,2 +0,0 @@
-// TODO: Replace with your actual Tumblr API key
-export const API_KEY = 'ru6b4z2sDMz7h0WyCULiNuqqgDfgubrdQZtZrVUkXQGkzFPTrF';
--- copy.js
+++
@@ -1,2 +0,0 @@
-// TODO: Replace with your actual Tumblr API key
-export const API_KEY = 'OAuth Consumer Key Goes Here';
